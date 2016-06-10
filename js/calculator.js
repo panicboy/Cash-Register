5da3882b113e5e1c295715aef89e449e20d86726
panicboy <asasaki@gmail.com>
2016-06-09 20:02:49 -1000

    partially working, full of test code

---
+++ js/calculator.js
@@ -0,0 +1,145 @@
+/**
+ * Declare a function named `calculatorModule`
+ * this function will have two private variables declared inside of it.
+ * @variable PRIVATE { Number } `memory`
+ * @variable PRIVATE { Number } `total`
+ * @return {object} `calculator` object that can be used
+ */
+
+ var calculatorModule = (function(){
+    // initialize variables
+    var total;
+    var memory = 0;
+    var history = [];
+
+
+  /**
+   * sets the `total` to the number passed in
+   * @param  { Number } x
+   * @return { Number }    current total
+   */
+    function doLoad(x){
+      addHistoryItem("load",x);
+      validateNumber(x);
+      total = x;
+    return total;
+    }
+
+
+  /**
+   * Return the value of `total`
+   * @return { Number }
+   */
+
+    function _getTotal(){
+      return total;
+    }
+
+
+  /**
+   * Sums the value passed in with `total`
+   * @param { Number } x
+   */
+    function doAdd(x){
+      addHistoryItem("add",x);
+      validateNumber(x);
+      total += x;
+    }
+
+
+  /**
+   * Subtracts the value passed in from `total`
+   * @param  { Number } x
+   */
+    function doSubtract(x){
+      addHistoryItem("subtract",x);
+      validateNumber(x);
+      total -= x;
+    }
+
+
+  /**
+   * Multiplies the value by `total`
+   * @param  { Number } x
+   */
+    function doMultiply(x){
+      addHistoryItem("multiply",x);
+      validateNumber(x);
+      total *= x;
+    }
+
+
+  /**
+   * Divides the value passing in by `total`
+   * @param  { Number } x
+   */
+    function doDivide(x){
+      addHistoryItem("divide",x);
+      validateNumber(x);
+      total /= x;
+    }
+
+
+  /**
+   * Return the value stored at `memory`
+   * @return { Number }
+   */
+    function getMemory(){
+      addHistoryItem("recall", memory);
+      return memory;
+    }
+
+
+  /**
+   * Stores the value of `total` to `memory`
+   */
+    function totalToMemory(){
+      addHistoryItem("total to memory",total);
+      memory = total;
+    }
+
+
+  /**
+   * Clear the value stored at `memory`
+   */
+    function clearMem(){
+      addHistoryItem("clearMemory",memory);
+      memory = 0;
+    }
+
+  /**
+   * Validation
+   */
+
+    function validateNumber(theNumber){
+      if(typeof theNumber != "number"){
+        throw Error('Error');
+      }
+    }
+
+    function addHistoryItem(operation,theNumber){
+      var historyItem = {action:operation, input:theNumber};
+      history.push(historyItem);
+    }
+
+    function clearLastHistoryItem(){
+      history.pop();
+    }
+
+    function clearHistory(){
+      history = [];
+    }
+
+      //
+    return {
+      load: doLoad,
+      add: doAdd,
+      subtract: doSubtract,
+      multiply: doMultiply,
+      divide: doDivide,
+      recallMemory: getMemory,
+      saveMemory: totalToMemory,
+      clearMemory: clearMem,
+      getTotal: _getTotal
+    };
+  });
--- js/cash_register.js
+++ js/cash_register.js
@@ -1,5 +1,12 @@
 var btnBox = document.getElementById('btnBox');
+var opBox = document.getElementById('opBox');
 var screen = document.getElementById('display');
+var oldScreen = document.getElementById('oldDisplay');
+var theNextFunction = document.getElementById('theFunction');
+var oldNum = null;
+var nextOperation = null;
+var calc = calculatorModule();
+console.log(calc);
 for (var i = 0; i < 10; i++) {
   var btn = document.createElement('button');
   btn.id = i;
@@ -7,8 +14,50 @@ for (var i = 0; i < 10; i++) {
   btn.innerHTML = i;
   btn.addEventListener('click', function(event) {
     //var sendVal = this.innerHTML;
-    screen.innerHTML = screen.innerHTML + this.innerHTML;
+    theNumber = Number(screen.innerHTML + this.innerHTML);
+    screen.innerHTML = theNumber;
   });
 
   btnBox.appendChild(btn);
 }
+
+var calcButtons = [{label: 'x', functionCall: 'doMultiply'},{label: '÷', functionCall: 'doDivide'},{label: '+', functionCall: 'doAdd'},{label: '-', functionCall: 'doSubtract'}];
+for (var i = 0; i < 4; i++) {
+var calcBtn = document.createElement('button');
+buttonProperties = calcButtons[i];
+calcBtn.innerHTML = buttonProperties.label;
+calcBtn.functionCall = buttonProperties.functionCall;
+calcBtn.addEventListener('click', function(event) {
+  oldScreen.innerHTML = screen.innerHTML;
+  theNextFunction.innerHTML = this.functionCall;
+    screen.innerHTML = 0;
+    /*else {
+    currentNum = Number(screen.innerHTML);
+    newNum = calc[nextOperation](oldNum,currentNum);
+    screen.innerHTML = calc[nextOperation](oldNum,currentNum);
+    oldNum = null;
+  }*/
+});
+opBox.appendChild(calcBtn);
+}
+
+equalBtn = document.createElement('button');
+equalBtn.innerHTML = '=';
+equalBtn.addEventListener('click', function(event){
+  num1 = Number(oldScreen.innerHTML);
+  num2 = Number(screen.innerHTML);
+  theOperation = theNextFunction.innerHTML;
+  if (theOperation = 'doMultiply') {
+    theResult = calc.multiply(num1,num2);
+  } else if (theOperation = 'doDivide') {
+    theResult = calc.divide(num1,num2);
+  } else if (theOperation = 'doAdd') {
+    theResult = calc.add(num1,num2);
+  } else if (theOperation = 'doSubtract') {
+    theResult = calc.subtract(num1,num2);
+  }
+  screen.innerHTML = theResult;
+  oldScreen.innerHTML = '';
+  theNextFunction.innerHTML = '';
+});
+opBox.appendChild(equalBtn);
